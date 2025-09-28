eda5e0c9a22eca47f4297f8122f612548fd26b16
Ramu <ramu.vuppanapalli@gmail.com>
2025-09-28 12:21:18 +0100

    proto changes of for these service

--- apps/backend/product-service/src/main.ts
+++ apps/backend/product-service/src/main.ts
@@ -1,13 +1,18 @@
 import { ProductServiceModule } from './app/product-service.module';
 import { bootstrapMicroservice } from '@my-product-app/backend-shared';
+import { protoPaths, protoPackages } from '@my-product-app/backend/proto';
 
 async function startProductService() {
-  console.log('startProductService calling');
+  console.log('Starting Product Service (gRPC)');
   await bootstrapMicroservice(ProductServiceModule, {
     hostEnv: 'MICROSERVICE_HOST',
-    portEnv: 'PRODUCT_SERVICE_MS_PORT', //  Use MS port
-    fallbackPort: 4001, //  Matches MS port
+    portEnv: 'PRODUCT_SERVICE_MS_PORT',
+    fallbackPort: 4001,
     serviceName: 'Product Service',
+    grpc: {
+      package: protoPackages.product,
+      protoPath: protoPaths.product,
+    },
   });
 }
 
--- apps/backend/workorder-service/src/main.ts
+++ apps/backend/workorder-service/src/main.ts
@@ -1,13 +1,18 @@
 import { WorkOrderServiceModule } from './app/workorder-service.module';
 import { bootstrapMicroservice } from '@my-product-app/backend-shared';
+import { protoPaths, protoPackages } from '@my-product-app/backend/proto';
 
 async function startWorkOrderService() {
-  console.log('startWorkOrderService calling');
+  console.log('Starting WorkOrder Service (gRPC)');
   await bootstrapMicroservice(WorkOrderServiceModule, {
     hostEnv: 'MICROSERVICE_HOST',
-    portEnv: 'WORKORDER_SERVICE_MS_PORT', // Use MS port
-    fallbackPort: 4005, // Matches MS port
+    portEnv: 'WORKORDER_SERVICE_MS_PORT',
+    fallbackPort: 4005,
     serviceName: 'WorkOrder Service',
+    grpc: {
+      package: protoPackages.workorder,
+      protoPath: protoPaths.workorder,
+    },
   });
 }
 
